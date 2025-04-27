2c0759cbc88cc6f547059c40737b38a074ddc230
Mack Stathis <mackstathis@gmail.com>
2025-04-26 20:57:29 -0400

    redis config for production

--- src/lib/queue/connection.ts
+++ src/lib/queue/connection.ts
@@ -5,6 +5,7 @@ console.log(`Using redis url: ${process.env.REDIS_URL}`)
 
 const connection = new IORedis(process.env.REDIS_URL || 'redis://localhost:6379', {
 	maxRetriesPerRequest: null,
+	family: 0
 })
 
 export { connection }
